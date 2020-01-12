a33a1df236c0a7d3867f8840a3dbeacc12758585
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-01-11 20:44:46 -0800

    minor corrections.

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const generateTeamHTML = require("./lib/generateTeamHTML");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
-// need function to write HTML file
-
 // this will contain the html cards for each employee
 const teamHTML = [];
 let mgrName = "";
@@ -18,32 +15,32 @@ let mgrName = "";
 let empId = 1;
 
 const welcome = [
-    { type: "input", message: "Team Summary - Enter Manager's Info then add Team Members. (press any key to continue)", name: "proceed"},  
+    { type: "input", message: "Team Summary - Enter Manager's Info then add Team Members. (press enter key to continue)", name: "proceed" },
 ];
 const empQuestions = [
-    { type: "input", message: "Enter Name", name: "name"},
+    { type: "input", message: "Enter Name", name: "name" },
     { type: "input", message: "Enter email address", name: "email" },
 ];
 const mgrQuestion = [
-    { type: "input", message: "Enter office number", name: "officeNumber" },  // manager
+    { type: "input", message: "Enter office number", name: "officeNumber" },
 ];
 const engQuestion = [
-    { type: "input", message: "Enter Engineer's GitHub username", name: "username" }, // engineers
+    { type: "input", message: "Enter Engineer's GitHub username", name: "username" },
 ];
 const intQuestion = [
-    { type: "input", message: "Where did Intern go to school?", name: "school" }, // interns
+    { type: "input", message: "Where did Intern go to school?", name: "school" },
 ];
 const addEmpQuestion = [
-    { type: "list",  message: "Do you want to add a team member?",choices: ["Engineer", "Intern","No"], name: "role"}
+    { type: "list", message: "Do you want to add a team member?", choices: ["Engineer", "Intern", "No"], name: "role" }
 ];
 
-async function getMgrData () {
+async function getMgrData() {
     try {
         let start = await inquirer.prompt(welcome);
         let empData = await inquirer.prompt(empQuestions);
         let mgrData = await inquirer.prompt(mgrQuestion);
-        const manager = new Manager(empData.name,empId,empData.email,mgrData.officeNumber);
-        empId ++;
+        const manager = new Manager(empData.name, empId, empData.email, mgrData.officeNumber);
+        empId++;
         mgrName = manager.name;
         teamHTML.push(manager.getHTML(manager));
         addTeamMember();
@@ -52,42 +49,42 @@ async function getMgrData () {
     };
 }
 
-async function addTeamMember () {
+async function addTeamMember() {
     try {
         let addEmployee = await inquirer.prompt(addEmpQuestion);
-            let role = addEmployee.role
-            if (role === "No") {
-                generateHTMLFile();
-            } else { getEmpData(role)};
+        let role = addEmployee.role
+        if (role === "No") {
+            generateHTMLFile();
+        } else { getEmpData(role) };
     } catch (err) {
         console.log(err);
     };
 }
 
-async function getEmpData (role) {
+async function getEmpData(role) {
     try {
         let empData = await inquirer.prompt(empQuestions);
         if (role === "Engineer") {
             let engData = await inquirer.prompt(engQuestion);
-            const engineer = new Engineer(empData.name,empId,empData.email,engData.username);
+            const engineer = new Engineer(empData.name, empId, empData.email, engData.username);
             teamHTML.push(engineer.getHTML(engineer));
         } else {
-        let intData = await inquirer.prompt(intQuestion);
-        const intern = new Intern(empData.name,empId,empData.email,intData.school);
-        teamHTML.push(intern.getHTML(intern));
+            let intData = await inquirer.prompt(intQuestion);
+            const intern = new Intern(empData.name, empId, empData.email, intData.school);
+            teamHTML.push(intern.getHTML(intern));
         };
-        empId ++;
+        empId++;
         addTeamMember();
     } catch (err) {
         console.log(err);
     };
 }
 
-function generateHTMLFile(){
-            const html = generateTeamHTML.generateTeamHTML(teamHTML,mgrName);
-            writeFileAsync("./output/team.html", html);
+function generateHTMLFile() {
+    const html = generateTeamHTML.generateTeamHTML(teamHTML, mgrName);
+    writeFileAsync("./output/team.html", html);
     console.log("File team.html was generated and placed in the output folder")
 }
 getMgrData();
 
-    
+
